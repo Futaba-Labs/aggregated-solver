d08543a1ac91cc9d2ba4fe97bc7e387338702c44
adachi_440 <tomoki1999314@gmail.com>
2024-11-01 00:29:13 +0700

    add dependecies

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import pluginJs from '@eslint/js';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import typescriptEslint from 'typescript-eslint';
+
+export default [{
+    files: ['**/*.{js,mjs,cjs,ts}']
+  },
+  {
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  pluginJs.configs.recommended,
+  ...typescriptEslint.configs.recommended,
+  eslintPluginPrettier,
+  {
+    rules: {
+      'no-console': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'prettier/prettier': 'error',
+    },
+  },
+];
